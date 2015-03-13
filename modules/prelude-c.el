9cfe51ed4ac2efd00068285821954f60540bd5c2
Thomas Gummerer <t.gummerer@gmail.com>
2015-03-13 21:05:01 +0100

    changes

--- modules/prelude-c.el
+++ modules/prelude-c.el
@@ -35,8 +35,7 @@
 (require 'prelude-programming)
 
 (defun prelude-c-mode-common-defaults ()
-  (setq c-default-style "k&r"
-        c-basic-offset 4)
+  (setq c-basic-offset 8)
   (c-set-offset 'substatement-open 0))
 
 (setq prelude-c-mode-common-hook 'prelude-c-mode-common-defaults)
